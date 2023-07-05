7d7608ee545ffc9f56071814d74efa7181b40b7e
Parth Kathuria <88901443+ParthDevTs@users.noreply.github.com>
2023-07-05 15:29:11 +0530

    added navlink styling

--- src/components/LeftSIdeBar.jsx
+++ src/components/LeftSIdeBar.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
-import { Link, useNavigate } from "react-router-dom"
+import { NavLink, useNavigate } from "react-router-dom"
 import { useAuthContext } from '../context/AuthContext';
 function LeftSIdeBar() {
     const navigate = useNavigate();
     const { userData } = useAuthContext();
+    const getActiveStyle = ({ isActive }) => ({
+        color: isActive ? "#FFB8B8" : "",
+    });
     return (
         <div className="left__nav bg-[#f1f1f1]   w-[18.75rem] px-[42px] flex flex-col items-center justify-between">
             <div className="feed__nav__links container w-[13.1875rem] h-[15rem] rounded-[10px] bg-white text-[#6C63FF] flex flex-col items-start justify-center px-[60px] gap-[20px]">
-                <Link className="navLinks text-left text-[1rem] font-semibold hover:text-[#9690ff] drop-shadow-xs" to="/">Home</Link>
-                <Link className="navLinks text-left text-[1rem] font-semibold hover:text-[#9690ff] drop-shadow-xs" to="/explore">Explore</Link>
-                <Link className="navLinks text-left text-[1rem] font-semibold hover:text-[#9690ff] drop-shadow-xs" to="/bookmarks">Bookmarks</Link>
-                <Link className="navLinks text-left text-[1rem] font-semibold hover:text-[#9690ff] drop-shadow-xs" to={`/profile/${userData._id}`}>Profile</Link>
+                <NavLink style={getActiveStyle} className="navLinks text-left text-[1rem] font-semibold hover:text-[#9690ff] drop-shadow-xs" to="/">Home</NavLink>
+                <NavLink style={getActiveStyle} className="navLinks text-left text-[1rem] font-semibold hover:text-[#9690ff] drop-shadow-xs" to="/explore">Explore</NavLink>
+                <NavLink style={getActiveStyle} className="navLinks text-left text-[1rem] font-semibold hover:text-[#9690ff] drop-shadow-xs" to="/bookmarks">Bookmarks</NavLink>
+                <NavLink style={getActiveStyle} className="navLinks text-left text-[1rem] font-semibold hover:text-[#9690ff] drop-shadow-xs" to={`/profile/${userData._id}`}>Profile</NavLink>
             </div>
             <div className="spacer flex flex-grow"></div>
             <div className="userContainer flex gap-4">
