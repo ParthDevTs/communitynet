d9a6bfdc24dfd8db9991637a2454c0f6f7fffe09
Parth Kathuria <88901443+ParthDevTs@users.noreply.github.com>
2023-07-08 14:09:06 +0530

    removed console.logs

--- src/components/editProfileDialog.jsx
+++ src/components/editProfileDialog.jsx
@@ -21,7 +21,7 @@ function EditProfileDialog({ profileData }) {
     }
 
     const changetoBase64 = (e) => {
-        console.log(e.target.files[0])
+
         var reader = new FileReader();
         reader.readAsDataURL(e.target.files[0])
         reader.onload = () => {
